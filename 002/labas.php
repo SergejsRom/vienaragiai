8b40fd0e9d2cd358e34e28e2efc6dffd38485975
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-05-05 11:37:20 +0300

    labas

---
+++ 002/labas.php
@@ -0,0 +1,27 @@
+<h1>Pats tu labas!</h1>
+
+
+<?php  
+/*
+$labas = 'valio';
+echo($labas);
+echo '<div>';
+*/
+// echo ($labas);
+
+$a = 6;
+
+echo 'Labas $a \n';
+?>
+
+
+<h1>Pats tu labas!</h1>
+
+<?php  
+
+echo 66 + 10;
+
+?>
+
+
+<h1>Pats tu labas!</h1>
