3520d567ebf6f10826a8f5c1d47029670c97867c
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-05-19 10:02:11 +0300

    state

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,18 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
-    const stebuklas = () => {
-        console.log('stebuklu stebuklas');
+    const [spalva, setSpalva] = useState('crimson');
+
+    const stebuklas = (a) => {
+        console.log('stebuklu stebuklas ' + a);
     }
+
+    const stebuklas1 = () => {
+        console.log('stebuklu stebuklas');
+        // const newColor = spalva === 'crimson' ? 'skyblue' : 'crimson';
+        setSpalva((oldColor) => oldColor === 'crimson' ? 'skyblue' : 'crimson');
+        // spalva = 'skyblue';
+        console.log(spalva);
+    }
+
 
     return (
         <div className="App">
             <header className="App-header">
-                <h1>State</h1>
+                <h1 style={{color:spalva}}>State</h1>
 
-
-                <button onClick={stebuklas}>Press Me!</button>
+                <button onClick={() => stebuklas('Abra-kadabra')}>Press WITH</button>
+                <button onClick={stebuklas1}>Press W/O</button>
             </header>
         </div>
     );
