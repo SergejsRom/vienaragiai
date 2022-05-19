6f8611e2189364a0b2c4bee1e432632e25d3423a
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-05-19 09:12:10 +0300

    stebuklas

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,18 +1,21 @@
 import './App.css';
-import Hello from './Components/009/Hello';
-import Kurmis from './Components/009/Kurmis';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Hello spalva="crimson" size={14}></Hello>
-        <Kurmis></Kurmis>
-        <Hello spalva="skyblue" size={22}></Hello>
-        <Hello spalva="yellow" size={33}></Hello>
-      </header>
-    </div>
-  );
+
+    const stebuklas = () => {
+        console.log('stebuklu stebuklas');
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>State</h1>
+
+
+                <button onClick={stebuklas}>Press Me!</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
