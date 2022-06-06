7d33a2ef9433662f46b346d2cd6058ede1ce5b9d
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-06-06 11:33:17 +0300

    app

---
+++ 017/bankas/public/index.php
@@ -0,0 +1,6 @@
+<?php
+use Bankas\App;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+App::start();
---
+++ 017/bankas/src/app/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Bankas\Controllers;
+use Bankas\App;
+
+
+class HomeController {
+
+
+    public function index() {
+        return App::view('home');
+    }
+}
