256cabf559d54d3e87996ef80bd5b14adb979676
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-06-08 10:17:07 +0300

    css

--- 017/bankas/src/views/home.php
+++ 017/bankas/src/views/home.php
@@ -4,7 +4,18 @@ require __DIR__ . '/top.php';
 ?>
 
 
+
 <h1>Sweet Home Alamaba</h1>
+
+<div class="home">
+        home
+    <div class="home__bin">
+            bin
+        <div class="home__bin__content">
+            content
+        </div>
+    </div>
+</div>
 
 <ul>
 <?php foreach($list as $value) : ?>
