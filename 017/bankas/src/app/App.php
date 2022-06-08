d2053293152d84ee5320e25c3bea1ac35cfda30c
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-06-08 09:52:28 +0300

    bankas

--- 017/bankas/src/app/App.php
+++ 017/bankas/src/app/App.php
@@ -5,19 +5,31 @@ use Bankas\Messages;
 class App {
 
         const DOMAIN = 'bankas.lt';
+        private static $html;
 
         public static function start() {
             session_start();
             Messages::init();
-            
+            ob_start();
             $uri = explode('/', $_SERVER['REQUEST_URI']);
             array_shift($uri);
             self::route($uri);
+            self::$html = ob_get_contents();
+            ob_end_clean();
+        }
+
+        public static function sent() {
+            echo self::$html;
         }
 
         public static function view(string $name, array $data = []) {
             extract($data);
-            return require __DIR__ .' /../views/'.$name.'.php';
+            require __DIR__ .' /../views/'.$name.'.php';
+        }
+
+        public static function json(array $data = []) {
+            header('Content-Type: application/json; charset=utf-8');
+            echo json_encode($data);
         }
 
         public static function redirect($url = '') {
@@ -30,6 +42,14 @@ class App {
 
             if (count($uri) == 1 && $uri[0] === '') {
                 return (new HomeController)->index( );
+            }
+
+            if ('GET' == $m && count($uri) == 1 && $uri[0] === 'json') {
+                return (new HomeController)->indexJson( );
+            }
+
+            if ('GET' == $m && count($uri) == 2 && $uri[0] === 'get-it') {
+                return (new HomeController)->getIt($uri[1]);
             }
 
             if ('GET' == $m && count($uri) == 1 && $uri[0] === 'forma') {
--- 017/bankas/src/app/Controllers/HomeController.php
+++ 017/bankas/src/app/Controllers/HomeController.php
@@ -7,12 +7,29 @@ use Bankas\Messages as M;
 class HomeController {
 
 
+    public function getIt($param) {
+
+        echo 'AAA: '.$param;
+    }
+
+
     public function index() {
         $list = [];
         for($i = 0; $i < 10; $i++) {
             $list[] = rand(1000, 9999);
         }
         return App::view('home', [
+            'title' => 'Alabama',
+            'list' => $list
+        ]);
+    }
+
+    public function indexJson() {
+        $list = [];
+        for($i = 0; $i < 10; $i++) {
+            $list[] = rand(1000, 9999);
+        }
+        return App::json([
             'title' => 'Alabama',
             'list' => $list
         ]);
