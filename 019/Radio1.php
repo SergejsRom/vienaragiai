0c67ba61ca16f2bcd77d52063c24a9e7ff38560b
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-06-08 11:17:07 +0300

    abstr

---
+++ 019/Radio1.php
@@ -0,0 +1,15 @@
+<?php
+
+class Radio1 extends Radio2 implements Planas {
+
+    public function goForIt(array $data) : array {
+        echo self::TU;
+        return [];
+    }
+
+    public function jaJa(int $data) : void {
+        
+    }
+
+
+}
