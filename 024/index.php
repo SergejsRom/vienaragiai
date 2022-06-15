08b54b792ef606db01a81105ece305444ed465f5
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-06-15 11:08:08 +0300

    api cache

--- 024/index.php
+++ 024/index.php
@@ -1,16 +1,28 @@
 <?php
+require __DIR__ . '/Cache.php';
 session_start();
+$cache = new Cache;
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-$ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, 'https://www.distance24.org/route.json?stops='.$_POST['from'].'|'.$_POST['to']);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $_SESSION['visual'] = 'CACHE';
+    $output = $cache->get();
+    if (null === $output) {
+        $_SESSION['visual'] = 'LIVE';
 
-$output = curl_exec($ch); // siuncia, laukiam, gaunam
+        $ch = curl_init();
 
-curl_close($ch); 
-
-$output = json_decode($output);
+        curl_setopt($ch, CURLOPT_URL, 'https://www.distance24.org/route.json?stops='.$_POST['from'].'|'.$_POST['to']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        
+        $output = curl_exec($ch); // siuncia, laukiam, gaunam
+        
+        curl_close($ch); 
+        
+        $output = json_decode($output);
+        $cache->set($output);
+    }
 
 $_SESSION['dist'] = $output->distance;
 $_SESSION['img1'] = $output->stops[0]->wikipedia->image;
@@ -23,8 +35,10 @@ die;
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $dist = $_SESSION['dist'] ?? '';
     $img1 = $_SESSION['img1'] ?? '';
-    unset($_SESSION['dist'], $_SESSION['img1']);
+    $visual = $_SESSION['visual'] ?? 'dar nera visualo';
+    unset($_SESSION['dist'], $_SESSION['img1'], $_SESSION['visual']);
     ?>
+    <h1 style="color:red;"><?= $visual ?></h1>
     <form action="" method="post">
         from: <input type="text" name="from">
         to: <input type="text" name="to">
